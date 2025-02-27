5f439cfea2260b4d169443c82edd900323a30de1
Jamenson Araujo <96190473+afjames@users.noreply.github.com>
2025-02-27 16:36:19 +0000

    altera o  arquivo `get.teste.js`

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,6 +3,6 @@ test("GET to /api/v1/status should return 200", async () => {
     "https://glorious-invention-x67976447qf65v9-3000.app.github.dev/api/v1/status",
   );
   expect(response.status).toBe(200);
-  const responseBody = await response.json();
+  const responseBody = await response.json;
   console.log(responseBody);
 });
