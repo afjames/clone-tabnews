d00c24e5ab6fc663b6fab41542aa8d0cb1bcc679
Jamenson Araujo <96190473+afjames@users.noreply.github.com>
2025-02-27 16:33:26 +0000

    altera o  arquivo `get.teste.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,7 +3,7 @@ import database from "infra/database.js";
 async function status(request, response) {
   const result = await database.query("SELECT 1 + 1 as SUM;");
   console.log(result);
-  response.status(200).json({ chave: "OI" });
+  response.status(200).json({ chave: "são acima da média" });
 }
 
 export default status;
