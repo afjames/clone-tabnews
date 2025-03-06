d9191d6c3684c5ab1bbc2145876535d8a3cce400
Jamenson Araujo <96190473+afjames@users.noreply.github.com>
2025-03-06 18:15:01 +0000

    finish `api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,8 +1,20 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch(
-    "https://glorious-invention-x67976447qf65v9-3000.app.github.dev/api/v1/status",
-  );
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-  const responseBody = await response.json;
-  console.log(responseBody);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const versionDatabase = responseBody.dependencies.database.version;
+  expect(versionDatabase).toEqual("16.0");
+
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toBeDefined();
+  expect(maxConnections).toEqual(100);
+
+  const opened_connections =
+    responseBody.dependencies.database.opened_connections;
+  expect(opened_connections).toEqual(1);
 });
